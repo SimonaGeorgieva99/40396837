d2f3931697a3df29ef6c9f4e6da240ed74a70e2b
Simona Georgieva <simona_georgieva99@abv.bg>
2020-10-27 15:16:08 +0000

    Did half of the home page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/home/')
 def home():
-	return render_template('navBar.html')
+	return render_template('homePage.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
