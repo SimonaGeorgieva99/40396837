4d227bf558761663d2ce0d23e6f17a66b87d5483
Simona Georgieva <simona_georgieva99@abv.bg>
2020-11-17 11:53:58 +0000

    Started html content part

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home():
 	return render_template('homePage.html')
 
+@app.route('/htmlIntro/')
+def htmlIntro():
+	return render_template('htmlIntro.html')
+
+@app.route('/htmlAttributes/')
+def htmlAttributes():
+	return render_template('htmlAttributes.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
