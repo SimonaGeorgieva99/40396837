e97665baa0f48612fe73f687d08414a8fb164b3e
Simona Georgieva <simona_georgieva99@abv.bg>
2020-11-22 17:05:18 +0000

    Added contacts page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/home/')
@@ -16,6 +16,27 @@ def htmlAttributes():
 @app.route('/htmlLists/')
 def htmlLists():
 	return render_template('htmlLists.html')
+
+@app.route('/htmlLayout/')
+def htmlLayout():
+	return render_template('htmlLayout.html')
+
+@app.route('/htmlTables/')
+def htmlTables():
+	return render_template('htmlTables.html')
+
+@app.route('/htmlForms/')
+def htmlForms():
+	return render_template('htmlForms.html')
+
+@app.route('/contacts/')
+def contacts():
+	return render_template('contacts.html')
+
+@app.route('/contacts/',methods=['POST'])
+def test():
+	mess=request.form['body']
+	return redirect('mailto:mon4eto@abv.bg'+str(mess))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
